637273e0b0b0ef63549a21209c722a59c860f76e
茭白 <jiaoxinwei@myweimai.com>
2017-06-13 17:14:56 +0800

    1：项目创建

---
+++ DesktopFunction/DesktopFunction/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DesktopFunction
+//
+//  Created by 茭白 on 2017/6/13.
+//  Copyright © 2017年 茭白. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DesktopFunction/DesktopFunction/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DesktopFunction
+//
+//  Created by 茭白 on 2017/6/13.
+//  Copyright © 2017年 茭白. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
